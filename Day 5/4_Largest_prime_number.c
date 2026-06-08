cfce46d3f9cdb0b8fabb71a5315c6b15e0abb906
MOHD NAWAZISH <mn4577650@gmail.com>
2026-06-08 19:54:32 +0530

    Add files via upload

---
+++ Day 5/4_Largest_prime_number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int n, largestPrimeFactor = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(int i=2; i<=n; i++){
+        while(n%i==0){
+            largestPrimeFactor = i;
+            n = n/i;
+        }
+    }
+    printf("Largest Prime Factor =%d", largestPrimeFactor);
+    return 0;
+}
