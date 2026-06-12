b529e34db52157523ce1077a1c4155d8ddcc6f43
MOHD NAWAZISH <mn4577650@gmail.com>
2026-06-12 17:59:31 +0530

    Add files via upload

---
+++ Day 11/output/2_maximum.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int maximum(int a, int b){
+    if(a>b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+
+int main(){
+    
+    int n1, n2, result;
+
+    printf("Enter two numbers :");
+    scanf("%d %d", &n1, &n2);
+
+    result = maximum(n1, n2);
+
+    printf("Maximum number = %d", result);
+
+    return 0;
+}
---
+++ Day 11/output/3_check_prime.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int isPrime(int n){
+
+    int i;
+
+    if(n<=1){
+    return 0;
+    }
+    for(int i=2; i<=n/2; i++){
+        if(n%i==0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(){
+     
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(isPrime(num)){
+        printf("%d is a Prime Number",num);
+    }
+    else{
+        printf("%d is not a prime number", num);
+    }
+    return 0;
+}
