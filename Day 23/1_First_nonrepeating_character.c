201b5f262937fc0fadc1b21df71b257d7dac0d61
MOHD NAWAZISH <mn4577650@gmail.com>
2026-06-23 08:20:18 +0530

    Add files via upload

---
+++ Day 23/1_First_nonrepeating_character.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int count[256] = {0};
+    printf("Enter a string: ");
+    gets(str);
+    
+    for (int i = 0; str[i] != '\0'; i++) {
+        count[str[i]]++;
+    }
+    
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (count[str[i]] == 1) {
+            printf("First non-repeating character is: %c\n", str[i]);
+            return 0;
+        }
+    }
+    
+    printf("All characters are repeating.\n");
+    return 0;
+}
