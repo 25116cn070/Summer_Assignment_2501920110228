93277d7ebeaab87aad14b74fa5f566635c407de4
MOHD NAWAZISH <mn4577650@gmail.com>
2026-06-30 08:18:21 +0530

    Add files via upload

---
+++ Day 30/1_Create_student_record_system_using_arrays&strings.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+    char names[n][50];
+    int marks[n];
+    
+    for (i = 0; i < n; i++) {
+        printf("Enter Name of student %d: ", i + 1);
+        scanf("%s", names[i]);
+        printf("Enter Marks of student %d: ", i + 1);
+        scanf("%d", &marks[i]);
+    }
+    
+    printf("\n--- Student Records ---\n");
+    for (i = 0; i < n; i++) {
+        printf("%s - %d marks\n", names[i], marks[i]);
+    }
+    return 0;
+}
